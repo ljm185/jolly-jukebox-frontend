5b5e71b26edcc511719faeb3aacc48e9b14767ad
github username <github email address>
2019-10-24 08:58:11 -0400

    minor change

--- src/components/SongDisplay.js
+++ src/components/SongDisplay.js
@@ -12,11 +12,11 @@ class SongDisplay extends Component {
                 <button className="closeButton" onClick={this.props.handleCloseSongClickProp}>Close Song</button><br/>
                 <p/>
                 <iframe title="musicVideo" width="560" height="315" src={videoSrc} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe><br/>
-                <p>If YouTube embed doesn't work, click <a href={this.props.displayedSong.url} target="_blank">HERE</a></p>
+                <p>If YouTube embed doesn't work, click <a href={this.props.displayedSong.url} target="_blank" rel="noopener noreferrer">HERE</a></p>
                 {/* <img className="dataImage" src={this.props.displayedSong.image} width="300" height="300" alt=""/> */}
                 <h1><strong>{this.props.displayedSong.title}</strong></h1>
                 <p>by {this.props.displayedSong.artist}</p>
-                <p>from the album <a href={this.props.displayedSong.image} target="_blank">{this.props.displayedSong.album} ({this.props.displayedSong.year})</a></p>
+                <p>from the album <a href={this.props.displayedSong.image} target="_blank" rel="noopener noreferrer">{this.props.displayedSong.album} ({this.props.displayedSong.year})</a></p>
                 <p>Genre: {this.props.displayedSong.genre.name}</p>
                 <p>Recommended Instrument: {this.props.displayedSong.instrument.name}</p>
                 {/* <iframe title="musicVideo" width="560" height="315" src={videoSrc} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
